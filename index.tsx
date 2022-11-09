520a87d230c3b5e689cb773e4d99decca2782143
nayarawatanuki <nayarawatanuki@gmail.com>
2022-11-09 00:05:42 -0300

    page.git

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from './styles/Home.module.css'
 
 
 import {
